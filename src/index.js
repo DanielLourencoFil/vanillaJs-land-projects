fd8a0a68c9fee42a5be772366ebb1acfc553299d
daniellourencoedu <daniellourencoedu@gmail.com>
2022-03-25 13:27:49 -0300

    sections components

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import SectionProjects from "./components/SectionProjects";
 import Hero from "./components/Hero";
 import Loading from "./components/Loading";
+import dataProjects from "./dataProjects";
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -12,7 +13,8 @@ ReactDOM.render(
 		})}
 
 		<Hero />
-		<App />
+		<SectionProjects data={dataProjects} title={"Basic Projects"} />
+		<SectionProjects data={dataProjects} title={"Courses Projects"} />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
